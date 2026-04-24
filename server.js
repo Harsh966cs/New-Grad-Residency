26a1a492346be2eee36db19e09e032bfbfd37d8d
Harsh966cs <120917444+Harsh966cs@users.noreply.github.com>
2026-04-25 04:50:10 +0530

    for updating size

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { isRelevant } = require("./utils");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.post("/", (req, res) => {
   const cases = req.body.cases;
